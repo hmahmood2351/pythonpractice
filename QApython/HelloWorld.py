0ee2e2a84e5b7a52a4e4f79760c4cb6e98cfd2e7
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-01 11:45:53 +0000

    Updated with last two exercises

--- QApython/HelloWorld.py
+++ QApython/HelloWorld.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+
+# inputs firstname lastname and prints it out 
+firstname = input("type in your first name: ")
+lastname = input("type in your last name: ")
+print(firstname + " " + lastname)
+
+
+#Adding 2 floats together and printing it out 
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+print(number1, "+", number2, "=", answer)
