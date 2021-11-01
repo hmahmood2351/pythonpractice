f1c750744b2e4059350fff76a95ec4cd520451b0
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-01 15:45:02 +0000

    Collections

---
+++ QApython/3Collections.py
@@ -0,0 +1,15 @@
+# dictionary composed of books and their respective authors
+
+books = {'1984':'George Orwell', 'Brave New World':'Aldous Huxley'}
+
+# querying author of 1984, using keys only for dicts
+
+print(books['1984'])
+
+# printing certain indexes of a list
+
+numberlist = [1,2,3]
+print(numberlist[0:2]) #gives us [1,2]
+
+#Bunch of different operatins relating to the data types, refer to documentation for more information
+#on usage of dicts, tuples, sets and lists
