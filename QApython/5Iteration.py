e8e0d734f3eb2304b1b4dc161d6c58754fc005ba
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-02 12:06:32 +0000

    While only

--- QApython/5Iteration.py
+++ QApython/5Iteration.py
@@ -12,9 +12,18 @@ for i in range(5, 11):
     
 # exercise branch
 
-for i in range(5): #asks for 5 names and prints them out with awesome following it 
-    name = input("What is your name? ")
+namelist = []
+while True:
+    name = input("What is your name?")
+    namelist.append(name)
     print(name, "is awesome!")
 
-for i in range (10, 21, 2): # prints out even numbers from 10 to 20
-    print(i)
+    if len(namelist) == 5:
+        break
+
+# for i in range(5): #asks for 5 names and prints them out with awesome following it 
+#     name = input("What is your name? ")
+#     print(name, "is awesome!")
+
+# for i in range (10, 21, 2): # prints out even numbers from 10 to 20
+#     print(i)
