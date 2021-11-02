053900b199400f143eae7acab325b5928084909f
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-02 20:30:25 +0000

    added info for conjugation

--- QApython/5.5intermediateconjugation.py
+++ QApython/5.5intermediateconjugation.py
@@ -1,3 +1,4 @@
 # A list of English words can be found here: http://www-personal.umich.edu/~jlawler/wordlist.
 # Find out how many different words you can make, from smaller valid sub-words.
 
+#answer to this one is in a separate repo - see Intermediateconjugation by hmahmood2351
