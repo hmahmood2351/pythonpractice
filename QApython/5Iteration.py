2db991894995fbc88d94c6312d4c5a56acdeb490
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-02 11:40:57 +0000

    exercise

---
+++ QApython/5Iteration.py
@@ -0,0 +1,20 @@
+# tutorial main branch
+
+count = 0 
+name = str(input("What is your name? "))
+
+while count < 5:
+    print(name, "is awesome!")
+    count += 1
+
+for i in range(5, 11):
+    print(i)
+
+# exercise branch (to be branched)
+
+for i in range(5): #asks for 5 names and prints them out with awesome following it 
+    name = input("What is your name? ")
+    print(name, "is awesome!")
+
+for i in range (10, 21, 2): # prints out even numbers from 10 to 20
+    print(i)
