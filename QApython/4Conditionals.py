3598def789301fcaf10132ccc6b5b608dd3b59db
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-02 09:58:03 +0000

    Exercise branch work

--- QApython/4Conditionals.py
+++ QApython/4Conditionals.py
@@ -9,3 +9,23 @@ elif devs_money < 10 and dev_can_play_smash:
     print("Dev is too poor to enter")
 else:
     print("Dev can't play smash")
+
+#exercise branch commit - to be merged with main
+
+mark = int(input("Type in your mark: "))
+if mark > 85:
+    print("Distinction")
+elif mark > 65 and mark < 85:
+    print("Pass")
+else:
+    print("Fail")
+
+#without elif
+
+if mark > 85:
+    print("Distinction")
+else:
+    if mark > 65 and mark < 85:
+        print("Pass")
+    else:
+        print("Fail")
