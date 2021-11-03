8eeac72ac50fa86363fd835e801852cd5b2b8fa8
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-03 16:55:30 +0000

    extra comments

--- QApython/5.5intermediateconjugation.py
+++ QApython/5.5intermediateconjugation.py
@@ -2,3 +2,4 @@
 # Find out how many different words you can make, from smaller valid sub-words.
 
 #answer to this one is in a separate repo - see Intermediateconjugation by hmahmood2351
+#not 100% completed but works to some extent - a technical victory?
