ccc23a2463528d6358f7bfdb1ccf07bfe7e87cd4
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-11-04 22:29:49 +0000

    challenges

---
+++ QApython/5.6intermediatenumberstowords.py
@@ -0,0 +1,9 @@
+# Challenge
+# The code should take in a number and respond with the word description for what that number is.
+
+# Example
+# Enter a number:
+# 123
+# One hundred and twenty-three
+
+# Refer to Intermediateconjugation by hmahmood2351 for file
